20ed3c27fa09859b4b9749779ce117517e393193
Nilesh Garg <nileshgarg0023@gmail.com>
2021-03-04 04:32:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-function takeANumber(currentLine, personName) {
-for(let i = 0; i < katzDeliLine.length; i++){
-  return `welcome,  ${personName[i]}.  You are number ${currentLine[i + 1]} in line.`
-}
-}
+var katzDeliLine = [];
