380eb99261160b0c85cb522b824143417d36d052
Nilesh Garg <nileshgarg0023@gmail.com>
2021-03-04 04:37:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ let takeANumber = (currentLine, name) => {
   currentLine.push(`${name}`);
   return `Welcome, ${name}. You are number ${currentLine.length} in line.`
 }
+
+
+let nowServing = currentLine => {
+  if (currentLine.length === 0) {
+    return 'There is nobody waiting to be served'
+  }
+}
